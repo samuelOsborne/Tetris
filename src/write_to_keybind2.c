c2e7ed0cd710523283e10dd07001b61278b70b33
villen_l <lucas.villeneuve@epitech.eu>
2016-03-19 14:51:15 +0000

    norme v3

--- src/write_to_keybind2.c
+++ src/write_to_keybind2.c
@@ -5,7 +5,7 @@
 ** Login   <samuel@epitech.net>
 ** 
 ** Started on  Mon Feb 29 17:51:01 2016 Samuel
-** Last update Fri Mar 18 09:26:55 2016 Samuel
+** Last update Sat Mar 19 14:46:13 2016 Lucas Villeneuve
 */
 
 #include "my.h"
@@ -31,7 +31,7 @@ void	put_next(t_keybinds *keybinds, char *word)
   keybinds->next = 0;
 }
 
-char	*put_void (char *a)
+char	*put_void(char *a)
 {
   return (a);
 }
